629fd902d21bf2ede29d9e7f72c5a89735aeb308
Rama Kocherlakota <Rama.Kocherlakota@coxautoinc.com>
2022-09-21 19:35:51 -0400

    allow specifying db

--- python/wordle/guess.py
+++ python/wordle/guess.py
@@ -4,16 +4,19 @@ import sys, json
 guess_scores = []
 hard_mode = False
 debug = False
+dbname = "wordle.sqlite"
 
-for arg in sys.argv:
+for arg in sys.argv[1:]:
     if arg.startswith("-"):
         if arg == "--hard":
             hard_mode = True
         if arg == "--debug":
             debug = True
-    if "=" in arg:
+        if arg.startswith("--dbname"):
+            dbname = arg.split("=")[1]
+    elif "=" in arg:
         guess_scores.append(arg.split("="))
 
-wordle = Wordle.Wordle(sqlite_folder="../db", sqlite_dbname = "wordle.sqlite", guess_scores = guess_scores, hard_mode=hard_mode, debug=debug)
+wordle = Wordle.Wordle(sqlite_folder="../db", sqlite_dbname = dbname, guess_scores = guess_scores, hard_mode=hard_mode, debug=debug)
 
 print(json.dumps(wordle.guess()))
--- python/wordle/solve.py
+++ python/wordle/solve.py
@@ -5,6 +5,7 @@ start_with = []
 hard_mode = False
 debug = False
 target = None
+dbname = "wordle.sqlite"
 
 for arg in sys.argv[1:]:
     if arg.startswith("-"):
@@ -14,6 +15,8 @@ for arg in sys.argv[1:]:
             debug = True
         if arg.startswith("--start"):
             start_with = arg.split("=")[1].split(",")
+        if arg.startswith("--dbname"):
+            dbname = arg.split("=")[1]
     else:
         target = arg
 
@@ -21,6 +24,6 @@ if not target:
     print("You must specify a target word!")
     sys.exit(1)
 
-wordle = Wordle.Wordle(sqlite_folder="../db", sqlite_dbname = "wordle.sqlite", hard_mode=hard_mode, debug=debug)
+wordle = Wordle.Wordle(sqlite_folder="../db", sqlite_dbname = dbname, hard_mode=hard_mode, debug=debug)
 
 print(json.dumps(wordle.solve(target, start_with=start_with)))
